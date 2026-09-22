834f38b1267feff022f400a747ae5fd9c6bc33ea
Nicholas Kutschke <nickkutschk25@gmail.com>
2026-09-22 19:43:59 -0400

    commented minimax character

--- team10/project1/minimaxcharacter.py
+++ team10/project1/minimaxcharacter.py
@@ -19,17 +19,20 @@ class MiniMaxCharacter(CharacterEntity):
         self.previous_position = (x, y)
         
     def count_move(self):
+        #if the character changed position, increment the total move counter
         position = (self.x, self.y)
         if position != self.previous_position:
             self.total_moves += 1
             self.previous_position = position
 
     def done(self, wrld):
+        #adds up the total moves the character made
         self.count_move()
         print("Final total moves:", self.total_moves)
         
 
     def do(self, wrld):
+        #Stuff for timing/performance evaluation
         self.count_move()
         me = wrld.me(self)
 
@@ -39,18 +42,26 @@ class MiniMaxCharacter(CharacterEntity):
 
         start = time.perf_counter()
 
+        #_____________________________________________________________________________
+        #Find a path to the goal using minimax with a depth of 2
         dx, dy = minimax.minimax(
-            wrld, self, depth=3
+            wrld, self, depth=2
         )
+        
+        self.move(dx, dy)
 
+        #_____________________________________________________________________________
+
+        
+        #Stuff for timing/performance evaluation
         speed = time.perf_counter() - start
         self.decision_times.append(speed)
 
+        total = sum(self.decision_times)
         mean = sum(self.decision_times) / len(self.decision_times)
         worst = max(self.decision_times)
 
         print("Current decision:", round(speed, 4), "s")
+        print("Total:", round(total, 4), "s")
         print("Mean:", round(mean, 4), "s")
         print("Worst:", round(worst, 4), "s")
-
-        self.move(dx, dy)
